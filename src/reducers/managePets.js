c5bfd29a8947747998774cbfa473f3dca1da0e7a
entcheva <christina.entcheva@gmail.com>
2017-02-12 16:40:19 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,22 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  if (action.type === 'ADD_PET') {
+    return ([...state], {pets: [...state.pets, action.payload]});
+  } else if (action.type === 'REMOVE_PET') {
+    return ([...state], {pets: state.pets.filter( pet => { if (pet.id !== action.payload) { return pet} })});
+  } else {
+    return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
-
+  var pets = state.pets.map( pet => { return `<li>${pet.name}</li>`} ).join(" ")
+  document.getElementById("container").innerHtml = `<ul>${pets}</ul>`
 }
